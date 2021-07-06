574f8d12e04a621611b94aa261ceff01cbb79ab3
Tim <sispo777@gmail.com>
2021-07-06 09:05:32 +0200

    Issue 7 - Refactored Cell to use programmatic UI + Updated layout

--- CampaignBrowser/Screens/CampaignsListing/Cells/CampaignCell.swift
+++ CampaignBrowser/Screens/CampaignsListing/Cells/CampaignCell.swift
@@ -1,6 +1,19 @@
 import UIKit
 import RxSwift
+import SnapKit
 
+/**
+ Style structure that may be used throughout the project to share styling variables.
+ */
+fileprivate struct Style {
+    struct Font {
+        let title = UIFont(name: "Helvetica Neue Bold", size: 18)
+        let subtitle = UIFont(name: "Hoefler Text", size: 12)
+    }
+    let font = Font()
+}
+
+fileprivate var style = Style()
 
 /**
  The cell which displays a campaign.
@@ -10,13 +23,13 @@ class CampaignCell: UICollectionViewCell {
     private let disposeBag = DisposeBag()
 
     /** Used to display the campaign's title. */
-    @IBOutlet private(set) weak var nameLabel: UILabel!
+    private(set) weak var nameLabel: UILabel!
 
     /** Used to display the campaign's description. */
-    @IBOutlet private(set) weak var descriptionLabel: UILabel!
+    private(set) weak var descriptionLabel: UILabel!
 
     /** The image view which is used to display the campaign's mood image. */
-    @IBOutlet private(set) weak var imageView: UIImageView!
+    private(set) weak var imageView: UIImageView!
 
     /** The mood image which is displayed as the background. */
     var moodImage: Observable<UIImage>? {
@@ -45,9 +58,71 @@ class CampaignCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupView()
 
         assert(nameLabel != nil)
         assert(descriptionLabel != nil)
         assert(imageView != nil)
+    }
+}
+
+private extension CampaignCell {
+    
+    func setupView() {
+        setupImageView()
+        setupTitleLabel()
+        setupDescriptionLabel()
+    }
+    
+    func setupImageView() {
+        let v = UIImageView()
+        contentView.addSubview(v)
+        
+        let aspectRatio: CGFloat = 4.0 / 3
+        
+        v.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.height.equalTo(contentView.snp.width).dividedBy(aspectRatio)
+        }
+        
+        v.contentMode = .scaleAspectFill
+        
+        imageView = v
+    }
+    
+    func setupTitleLabel() {
+        let l = UILabel()
+        contentView.addSubview(l)
+        
+        l.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom)
+            make.left.equalToSuperview().offset(8)
+            make.right.equalToSuperview().offset(-8)
+        }
+        
+        l.font = style.font.title
+        l.numberOfLines = 2
+        l.textAlignment = .natural
+        
+        nameLabel = l
+    }
+    
+    func setupDescriptionLabel() {
+        let l = UILabel()
+        contentView.addSubview(l)
+        
+        l.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(8)
+            make.right.equalToSuperview().offset(-8)
+        }
+        
+        l.font = style.font.subtitle
+        l.numberOfLines = 0
+        l.textAlignment = .justified
+        
+        descriptionLabel = l
     }
 }
